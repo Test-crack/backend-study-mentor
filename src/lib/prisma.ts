0099658ac4d70959d7a1cbc0d8230c7834622cc7
Sarthakyadav98 <s9808927657@gmail.com>
2025-12-07 15:23:09 +0530

    Fix unstable version issue for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,6 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
