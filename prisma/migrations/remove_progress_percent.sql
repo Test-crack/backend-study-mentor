f2be3f5cca026c63f59440e8e9d2bc3c5c496d81
Sarthakyadav98 <s9808927657@gmail.com>
2026-07-27 03:21:18 +0530

    chore: baseline initial migration for existing schema

--- prisma/migrations/remove_progress_percent.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Remove progress_percent from UserModuleProgress
--- This column is now calculated on-the-fly based on content completion
-
-ALTER TABLE "UserModuleProgress" 
-DROP COLUMN IF EXISTS "progress_percent";
